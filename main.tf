ce7dcfd875085888ccbe96fa5373220811bedaf0
Igor Rodionov <goruha@gmail.com>
2019-11-25 20:16:10 +0600

    Initial commit

--- main.tf
+++ main.tf
@@ -70,14 +70,14 @@ data "aws_launch_configuration" "bastions" {
 
 locals {
   nodes = { for autoscale_group in data.aws_autoscaling_group.nodes :
-    autoscale_group.name => merge(autoscale_group, { launch_configuration = data.aws_launch_configuration.nodes[autoscale_group["launch_configuration"]] })
+    autoscale_group.name => merge(autoscale_group, { launch_configuration = data.aws_launch_configuration.nodes[autoscale_group.launch_configuration] })
   }
 
   masters = { for autoscale_group in data.aws_autoscaling_group.masters :
-    autoscale_group.name => merge(autoscale_group, { launch_configuration = data.aws_launch_configuration.masters[autoscale_group["launch_configuration"]] })
+    autoscale_group.name => merge(autoscale_group, { launch_configuration = data.aws_launch_configuration.masters[autoscale_group.launch_configuration] })
   }
 
   bastions = { for autoscale_group in data.aws_autoscaling_group.bastions :
-    autoscale_group.name => merge(autoscale_group, { launch_configuration = data.aws_launch_configuration.bastions[autoscale_group["launch_configuration"]] })
+    autoscale_group.name => merge(autoscale_group, { launch_configuration = data.aws_launch_configuration.bastions[autoscale_group.launch_configuration] })
   }
 }
