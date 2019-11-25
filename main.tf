a940e29d41ed0c5c53d87e0e2aeb92a514cc5dfd
Igor Rodionov <goruha@gmail.com>
2019-11-25 20:13:54 +0600

    Initial commit

--- main.tf
+++ main.tf
@@ -44,40 +44,40 @@ data "aws_autoscaling_group" "nodes" {
 }
 
 data "aws_autoscaling_group" "masters" {
-  for_each = toset(data.aws_autoscaling_groups.masters.names)
-  name     = each.key
+  count = length(data.aws_autoscaling_groups.masters.names)
+  name  = element(data.aws_autoscaling_groups.masters.names, count.index)
 }
 
 data "aws_autoscaling_group" "bastions" {
-  for_each = toset(data.aws_autoscaling_groups.bastions.names)
+  count = length(data.aws_autoscaling_groups.masters.names)
+  name  = element(data.aws_autoscaling_groups.masters.names, count.index)
+}
+
+data "aws_launch_configuration" "nodes" {
+  for_each = toset(data.aws_autoscaling_group.nodes.*.launch_configuration)
   name     = each.key
 }
 
-//data "aws_launch_configuration" "nodes" {
-//  for_each = toset(data.aws_autoscaling_group.nodes[*].launch_configuration)
-//  name     = each.key
-//}
-//
-//data "aws_launch_configuration" "masters" {
-//  for_each = toset(data.aws_autoscaling_group.masters[*].launch_configuration)
-//  name     = each.key
-//}
-//
-//data "aws_launch_configuration" "bastions" {
-//  for_each = toset(data.aws_autoscaling_group.bastions[*].launch_configuration)
-//  name     = each.key
-//}
-//
-//locals {
-//  nodes = { for autoscale_group_name, autoscale_group in data.aws_autoscaling_group.nodes :
-//    autoscale_group_name => merge(autoscale_group, { launch_configuration = data.aws_launch_configuration.nodes[autoscale_group["launch_configuration"]] })
-//  }
-//
-//  masters = { for autoscale_group_name, autoscale_group in data.aws_autoscaling_group.masters :
-//    autoscale_group_name => merge(autoscale_group, { launch_configuration = data.aws_launch_configuration.masters[autoscale_group["launch_configuration"]] })
-//  }
-//
-//  bastions = { for autoscale_group_name, autoscale_group in data.aws_autoscaling_group.bastions :
-//    autoscale_group_name => merge(autoscale_group, { launch_configuration = data.aws_launch_configuration.bastions[autoscale_group["launch_configuration"]] })
-//  }
-//}
+data "aws_launch_configuration" "masters" {
+  for_each = toset(data.aws_autoscaling_group.nodes.*.launch_configuration)
+  name     = each.key
+}
+
+data "aws_launch_configuration" "bastions" {
+  for_each = toset(data.aws_autoscaling_group.nodes.*.launch_configuration)
+  name     = each.key
+}
+
+locals {
+  nodes = { for autoscale_group in data.aws_autoscaling_group.nodes :
+    autoscale_group.name => merge(autoscale_group, { launch_configuration = data.aws_launch_configuration.nodes[autoscale_group["launch_configuration"]] })
+  }
+
+  masters = { for autoscale_group in data.aws_autoscaling_group.masters :
+    autoscale_group.name => merge(autoscale_group, { launch_configuration = data.aws_launch_configuration.masters[autoscale_group["launch_configuration"]] })
+  }
+
+  bastions = { for autoscale_group in data.aws_autoscaling_group.bastions :
+    autoscale_group.name => merge(autoscale_group, { launch_configuration = data.aws_launch_configuration.bastions[autoscale_group["launch_configuration"]] })
+  }
+}
