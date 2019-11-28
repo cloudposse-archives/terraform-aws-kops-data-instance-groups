c5594dcce5d0469b1617df09c4ff66aeddaeefa2
Igor Rodionov <goruha@gmail.com>
2019-11-28 22:47:06 +0600

    Get availiability zones

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "nodes" {
-  value       = local.nodes
+  value       = flatten(data.aws_autoscaling_groups.nodes.*.names)
   description = "Nodes autoscale groups + launch configurations"
 }
 
 output "masters" {
-  value       = local.masters
+  value       = flatten(data.aws_autoscaling_groups.masters.*.names)
   description = "Masters autoscale groups + launch configurations"
 }
 
 output "bastions" {
-  value       = local.bastions
+  value       = flatten(data.aws_autoscaling_groups.bastions.*.names)
   description = "Bastions autoscale groups + launch configurations"
 }
