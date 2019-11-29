b711dbff31544da7b0581a0ef01fc3af0cd2ca4d
Igor Rodionov <goruha@gmail.com>
2019-11-29 16:02:52 +0600

    Fix outputs

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,23 @@ output "bastions" {
   value       = flatten(data.aws_autoscaling_groups.bastions.*.names)
   description = "Bastions autoscale groups + launch configurations"
 }
+
+output "nodes_tags" {
+  value       = local.node_tags
+  description = "Nodes autoscale groups + launch configurations"
+}
+
+output "masters_tags" {
+  value       = local.master_tags
+  description = "Masters autoscale groups + launch configurations"
+}
+
+output "bastions_tags" {
+  value       = local.bastion_tags
+  description = "Bastions autoscale groups + launch configurations"
+}
+
+output "common_tags" {
+  value       = local.common_tags
+  description = "Nodes autoscale groups + launch configurations"
+}
